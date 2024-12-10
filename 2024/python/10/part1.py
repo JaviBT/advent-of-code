ffa720702eaded818d1734b38231b114f7c577df
JaviBT <francisco.barranco@estudiante.uam.es>
2024-12-10 06:26:58 +0100

    Day 10 - Advent of Code 2024

---
+++ 2024/python/10/part1.py
@@ -0,0 +1,47 @@
+raw = open('input.dat').readlines()
+raw = [line.strip() for line in raw]
+
+if False:
+    raw = '''
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+'''
+    raw = raw.strip().split('\n')
+
+grid = [[int(char) if char.isdigit() else char for char in line] for line in raw]
+n, m = len(grid), len(grid[0])
+
+dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+def calc_trailheads(grid, i, j):
+    if (i, j) in memo:
+        return 0
+    memo.add((i, j))
+
+    if (grid[i][j] == 9):
+        return 1
+    if (grid[i][j] == '.'):
+        return 0
+    
+    score = 0
+    for dx, dy in dir:
+        if i + dx in range(n) and j + dy in range(m) and grid[i + dx][j + dy] == grid[i][j] + 1:
+            score += calc_trailheads(grid, i + dx, j + dy)
+    
+    return score
+
+
+score = 0
+for i in range(n):
+    for j in range(m):
+        memo = set()
+        if grid[i][j] == 0:
+            score += calc_trailheads(grid, i, j)
+
+print(score)
--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Welcome to my Advent of Code solutions!
 | Day | Problem | Solution (Part 1) | Solution (Part 2) |
 |-----|---------|-------------------|-------------------|
 | 1 | [Day 1: Historian Hysteria](https://adventofcode.com/2024/day/1) | [Part 1 Solution](./2024/python/1/part1.py) | [Part 2 Solution](./2024/python/1/part2.py) |
+| 10 | [Day 10: Hoof It](https://adventofcode.com/2024/day/10) | [Part 1 Solution](./2024/python/10/part1.py) | [Part 2 Solution](./2024/python/10/part2.py) |
 | 2 | [Day 2: Red-Nosed Reports](https://adventofcode.com/2024/day/2) | [Part 1 Solution](./2024/python/2/part1.py) | [Part 2 Solution](./2024/python/2/part2.py) |
 | 3 | [Day 3: Mull It Over](https://adventofcode.com/2024/day/3) | [Part 1 Solution](./2024/python/3/part1.py) | [Part 2 Solution](./2024/python/3/part2.py) |
 | 4 | [Day 4: Ceres Search](https://adventofcode.com/2024/day/4) | [Part 1 Solution](./2024/python/4/part1.py) | [Part 2 Solution](./2024/python/4/part2.py) |
