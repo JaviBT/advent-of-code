3c9e1072fa82b5c087a4cc178cbafe09f6317182
javibarranco <javibarranco@google.com>
2025-12-03 08:24:41 +0100

    Correction to Day 3 AoC 2025 Part 1 in Python

--- 2025/python/3/part1.py
+++ 2025/python/3/part1.py
@@ -6,10 +6,13 @@ total = 0
 for line in raw:
     line = line.strip()
 
-    first, first_idx = max((int(line[i]), i) for i in range(len(line)))
-    second, second_idx = max((int(line[i]), i) for i in range(len(line)) if i >= first_idx)
+    n = len(line)
 
-    total += int(str(first) + str(second))
+    first, first_idx = max((int(line[i]), -i) for i in range(n) if i < n - 1)
+    second, second_idx = max((int(line[i]), -i) for i in range(n) if i > -first_idx and i < n)
+
+    max_str = str(first) + str(second)
+    total += int(max_str)
 
 print(total)
         
